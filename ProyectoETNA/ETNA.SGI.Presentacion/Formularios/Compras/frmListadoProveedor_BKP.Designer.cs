f3229ae77df2d39c1e16a9ae5b6b420cbd1ab569
eddy12241 <edinson.rojas.villarreyes@gmail.com>
2015-05-14 17:23:28 -0500

    14052015_1723

---
+++ ProyectoETNA/ETNA.SGI.Presentacion/Formularios/Compras/frmListadoProveedor_BKP.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace ETNA.SGI.Presentacion.Formularios.Compras
+{
+    partial class frmListadoProveedor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label11 = new System.Windows.Forms.Label();
+            this.groupBox6 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblRazonSocial = new System.Windows.Forms.Label();
+            this.txtRazonSocial = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.codProveedor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.razonSocial = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.direccion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.telefono = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fechaRegistro = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.email = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ruc = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.observacion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox6.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label11
+            // 
+            this.label11.BackColor = System.Drawing.Color.White;
+            this.label11.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label11.Location = new System.Drawing.Point(6, 12);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(730, 29);
+            this.label11.TabIndex = 0;
+            this.label11.Text = "Lista de Proveedores";
+            this.label11.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // groupBox6
+            // 
+            this.groupBox6.Controls.Add(this.label11);
+            this.groupBox6.Location = new System.Drawing.Point(12, 12);
+            this.groupBox6.Name = "groupBox6";
+            this.groupBox6.Size = new System.Drawing.Size(742, 49);
+            this.groupBox6.TabIndex = 101;
+            this.groupBox6.TabStop = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.codProveedor,
+            this.razonSocial,
+            this.direccion,
+            this.telefono,
+            this.fechaRegistro,
+            this.email,
+            this.ruc,
+            this.observacion});
+            this.dataGridView1.Location = new System.Drawing.Point(11, 187);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(732, 102);
+            this.dataGridView1.TabIndex = 102;
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Location = new System.Drawing.Point(12, 158);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(121, 23);
+            this.btnNuevo.TabIndex = 103;
+            this.btnNuevo.Text = "Añadir Proveedor";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblRazonSocial);
+            this.groupBox1.Controls.Add(this.txtRazonSocial);
+            this.groupBox1.Controls.Add(this.btnBuscar);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.Color.Navy;
+            this.groupBox1.Location = new System.Drawing.Point(12, 67);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(742, 85);
+            this.groupBox1.TabIndex = 104;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Criterios de Búsqueda";
+            // 
+            // lblRazonSocial
+            // 
+            this.lblRazonSocial.AutoSize = true;
+            this.lblRazonSocial.Location = new System.Drawing.Point(10, 40);
+            this.lblRazonSocial.Name = "lblRazonSocial";
+            this.lblRazonSocial.Size = new System.Drawing.Size(86, 13);
+            this.lblRazonSocial.TabIndex = 105;
+            this.lblRazonSocial.Text = "Razón Social:";
+            // 
+            // txtRazonSocial
+            // 
+            this.txtRazonSocial.Location = new System.Drawing.Point(125, 34);
+            this.txtRazonSocial.Name = "txtRazonSocial";
+            this.txtRazonSocial.Size = new System.Drawing.Size(146, 20);
+            this.txtRazonSocial.TabIndex = 104;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Image = global::ETNA.SGI.Presentacion.Properties.Resources.Buscar;
+            this.btnBuscar.Location = new System.Drawing.Point(676, 16);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(55, 57);
+            this.btnBuscar.TabIndex = 103;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // codProveedor
+            // 
+            this.codProveedor.HeaderText = "Código";
+            this.codProveedor.Name = "codProveedor";
+            this.codProveedor.ReadOnly = true;
+            // 
+            // razonSocial
+            // 
+            this.razonSocial.HeaderText = "Razon Social";
+            this.razonSocial.Name = "razonSocial";
+            this.razonSocial.ReadOnly = true;
+            // 
+            // direccion
+            // 
+            this.direccion.HeaderText = "Dirección";
+            this.direccion.Name = "direccion";
+            this.direccion.ReadOnly = true;
+            // 
+            // telefono
+            // 
+            this.telefono.HeaderText = "Telefono";
+            this.telefono.Name = "telefono";
+            this.telefono.ReadOnly = true;
+            // 
+            // fechaRegistro
+            // 
+            this.fechaRegistro.HeaderText = "Fecha Registro";
+            this.fechaRegistro.Name = "fechaRegistro";
+            this.fechaRegistro.ReadOnly = true;
+            // 
+            // email
+            // 
+            this.email.HeaderText = "Email";
+            this.email.Name = "email";
+            this.email.ReadOnly = true;
+            // 
+            // ruc
+            // 
+            this.ruc.HeaderText = "RUC";
+            this.ruc.Name = "ruc";
+            this.ruc.ReadOnly = true;
+            // 
+            // observacion
+            // 
+            this.observacion.HeaderText = "Observación";
+            this.observacion.Name = "observacion";
+            this.observacion.ReadOnly = true;
+            // 
+            // frmListadoProveedor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(766, 312);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnNuevo);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox6);
+            this.Name = "frmListadoProveedor";
+            this.Text = "Lista de Proveedores";
+            this.Load += new System.EventHandler(this.frmListadoProveedor_Load);
+            this.groupBox6.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.GroupBox groupBox6;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtRazonSocial;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Label lblRazonSocial;
+        private System.Windows.Forms.DataGridViewTextBoxColumn codProveedor;
+        private System.Windows.Forms.DataGridViewTextBoxColumn razonSocial;
+        private System.Windows.Forms.DataGridViewTextBoxColumn direccion;
+        private System.Windows.Forms.DataGridViewTextBoxColumn telefono;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fechaRegistro;
+        private System.Windows.Forms.DataGridViewTextBoxColumn email;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ruc;
+        private System.Windows.Forms.DataGridViewTextBoxColumn observacion;
+    }
+}
---
+++ ProyectoETNA/ETNA.SGI.Presentacion/Formularios/Compras/frmListadoProveedor_BKP.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using ETNA.SGI.Bussiness.Compras;
+using ETNA.SGI.Entity.Compras;
+using ETNA.SGI.Utils;
+using System.Globalization;
+
+namespace ETNA.SGI.Presentacion.Formularios.Compras
+{
+    public partial class frmListadoProveedor : Form
+    {
+        public frmListadoProveedor()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            Formularios.Compras.frmProveedor frm = new frmProveedor();
+            frm.ShowDialog();
+        }
+
+
+        private BProveedor bProveedor = BProveedor.getInstance();
+        EProveedor proveedor = new EProveedor();
+
+        private void frmListadoProveedor_Load(object sender, EventArgs e)
+        {
+
+            DataTable tblDetalle = new DataTable();
+            
+           tblDetalle =  bProveedor.DGetAllProveedor(proveedor);
+
+           dataGridView1.DataSource = tblDetalle;
+
+           /* DataTable tblDetalle = new DataTable();
+            tblDetalle.Columns.Add(new DataColumn("1", typeof(string)));
+            tblDetalle.Columns.Add(new DataColumn("2", typeof(string)));
+            tblDetalle.Columns.Add(new DataColumn("3", typeof(string)));
+
+            DataRow fila = tblDetalle.NewRow();
+            fila["1"] = "D0001";
+            fila["2"] = "Empresas Exportadoras";
+            fila["3"] = "Ver documento";
+            tblDetalle.Rows.Add(fila);
+
+            fila = tblDetalle.NewRow();
+            fila["1"] = "D0002";
+            fila["2"] = "Eventos Comerciales";
+            fila["3"] = "Ver documento";
+            tblDetalle.Rows.Add(fila);
+
+            fila = tblDetalle.NewRow();
+            fila["1"] = "D0003";
+            fila["2"] = "Requisitos de Calidad e Inocuidad";
+            fila["3"] = "Ver documento";
+            tblDetalle.Rows.Add(fila); */
+
+            
+        }
+
+
+     }
+}
