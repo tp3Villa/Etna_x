85cef289e0a999484ac415c08e97ee7c693be591
eddy12241 <edinson.rojas.villarreyes@gmail.com>
2015-05-14 01:37:34 -0500

    Proveedor

--- ProyectoETNA/ETNA.SGI.Presentacion/Formularios/Compras/frmActualizarProveedor.Designer.cs
+++ ProyectoETNA/ETNA.SGI.Presentacion/Formularios/Compras/frmActualizarProveedor.Designer.cs
@@ -42,7 +42,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(292, 262);
+            this.ClientSize = new System.Drawing.Size(781, 329);
             this.Controls.Add(this.txtRazonSocial);
             this.Name = "frmActualizarProveedor";
             this.Text = "frmActualizarProveedor";
