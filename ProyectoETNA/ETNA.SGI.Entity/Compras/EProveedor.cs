7d4760b92ac9dc9ec402963fc7a212dbbf01ce47
luisfp89 <luisfp89@gmail.com>
2015-05-14 00:31:50 -0500

    Add Entity EProveedor

---
+++ ProyectoETNA/ETNA.SGI.Entity/Compras/EProveedor.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ETNA.SGI.Entity.Exportacion
+{
+    public class EProveedor
+    {
+        private int codProveedor;
+
+        public int CodProveedor
+        {
+            get { return codProveedor; }
+            set { codProveedor = value; }
+        }
+        private string razonSocial;
+
+        public string RazonSocial
+        {
+            get { return razonSocial; }
+            set { razonSocial = value; }
+        }
+
+        private string direccion;
+
+        public string Direccion
+        {
+            get { return direccion; }
+            set { direccion = value; }
+        }
+
+        private int telefono;
+
+        public int Telefono
+        {
+            get { return telefono; }
+            set { telefono = value; }
+        }
+
+        private DateTime fechaRegistro;
+
+        public DateTime FechaRegistro
+        {
+            get { return fechaRegistro; }
+            set { fechaRegistro = value; }
+        }
+
+        private string email;
+
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        private int ruc;
+
+        public int Ruc
+        {
+            get { return ruc; }
+            set { ruc = value; }
+        }
+
+        private string observacion;
+
+        public string Observacion
+        {
+            get { return observacion; }
+            set { observacion = value; }
+        }
+    }
+}
