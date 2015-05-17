ba51ba01e17c98428be15ce2a09893d09bd96d61
luisfp89 <luisfp89@gmail.com>
2015-05-17 15:36:49 -0500

    .

---
+++ ProyectoETNA/ETNA.SGI.Presentacion/Formularios/Compras/frmListadoReqCotizacion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ETNA.SGI.Presentacion.Formularios.Compras
+{
+    public partial class frmListadoReqCotizacion : Form
+    {
+        public frmListadoReqCotizacion()
+        {
+            InitializeComponent();
+        }
+    }
+}
