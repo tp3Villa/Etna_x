312d9df87edb4ee26e15d73ba17c511c448d7360
luisfp89 <luisfp89@gmail.com>
2015-05-16 20:41:55 -0500

    frmCotizacion

---
+++ ProyectoETNA/ETNA.SGI.Presentacion/Formularios/Compras/frmCotizacion.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ETNA.SGI.Presentacion.Formularios.Compras
+{
+    public partial class frmCotizacion : Form
+    {
+        public frmCotizacion()
+        {
+            InitializeComponent();
+        }
+
+        private void frmCotizacion_Load(object sender, EventArgs e)
+        {
+
+        }
+
+ 
+
+
+
+
+
+   
+    }
+}
