964dfd0839c4f8da0ad1240350f0163bb78966bd
luisfp89 <luisfp89@gmail.com>
2015-05-17 15:54:02 -0500

    .

--- ProyectoETNA/ETNA.SGI.Presentacion/Formularios/Compras/frmListadoReqCotizacion.Designer.cs
+++ ProyectoETNA/ETNA.SGI.Presentacion/Formularios/Compras/frmListadoReqCotizacion.Designer.cs
@@ -31,14 +31,16 @@
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.btnBuscar = new System.Windows.Forms.Button();
-            this.txtDescripcion = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.groupBox6 = new System.Windows.Forms.GroupBox();
             this.lblBusqueda = new System.Windows.Forms.Label();
             this.codRequerimiento = new System.Windows.Forms.DataGridViewLinkColumn();
             this.desCategoria = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.fechaRegistro = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.observacion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cboCategoria = new System.Windows.Forms.ComboBox();
+            this.txtRequerimiento = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.groupBox6.SuspendLayout();
@@ -57,19 +59,21 @@
             this.dataGridView1.Location = new System.Drawing.Point(16, 152);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.Size = new System.Drawing.Size(501, 146);
+            this.dataGridView1.Size = new System.Drawing.Size(597, 146);
             this.dataGridView1.TabIndex = 67;
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.btnBuscar);
-            this.groupBox1.Controls.Add(this.txtDescripcion);
             this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtRequerimiento);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.cboCategoria);
+            this.groupBox1.Controls.Add(this.btnBuscar);
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox1.ForeColor = System.Drawing.Color.Navy;
             this.groupBox1.Location = new System.Drawing.Point(16, 64);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(501, 82);
+            this.groupBox1.Size = new System.Drawing.Size(597, 82);
             this.groupBox1.TabIndex = 66;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Criterios de búsqueda";
@@ -77,7 +81,7 @@
             // btnBuscar
             // 
             this.btnBuscar.Image = global::ETNA.SGI.Presentacion.Properties.Resources.Buscar;
-            this.btnBuscar.Location = new System.Drawing.Point(361, 19);
+            this.btnBuscar.Location = new System.Drawing.Point(475, 19);
             this.btnBuscar.Name = "btnBuscar";
             this.btnBuscar.Size = new System.Drawing.Size(75, 43);
             this.btnBuscar.TabIndex = 2;
@@ -85,28 +89,12 @@
             this.btnBuscar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.btnBuscar.UseVisualStyleBackColor = true;
             // 
-            // txtDescripcion
-            // 
-            this.txtDescripcion.Location = new System.Drawing.Point(121, 27);
-            this.txtDescripcion.Name = "txtDescripcion";
-            this.txtDescripcion.Size = new System.Drawing.Size(215, 20);
-            this.txtDescripcion.TabIndex = 1;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(10, 32);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(78, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Descripción:";
-            // 
             // groupBox6
             // 
             this.groupBox6.Controls.Add(this.lblBusqueda);
             this.groupBox6.Location = new System.Drawing.Point(16, 2);
             this.groupBox6.Name = "groupBox6";
-            this.groupBox6.Size = new System.Drawing.Size(501, 56);
+            this.groupBox6.Size = new System.Drawing.Size(597, 56);
             this.groupBox6.TabIndex = 65;
             this.groupBox6.TabStop = false;
             // 
@@ -117,7 +105,7 @@
             this.lblBusqueda.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
             this.lblBusqueda.Location = new System.Drawing.Point(6, 16);
             this.lblBusqueda.Name = "lblBusqueda";
-            this.lblBusqueda.Size = new System.Drawing.Size(489, 29);
+            this.lblBusqueda.Size = new System.Drawing.Size(585, 29);
             this.lblBusqueda.TabIndex = 1;
             this.lblBusqueda.Text = "Búsqueda de Requerimiento de Compra";
             this.lblBusqueda.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -152,11 +140,47 @@
             this.observacion.Name = "observacion";
             this.observacion.ReadOnly = true;
             // 
+            // cboCategoria
+            // 
+            this.cboCategoria.FormattingEnabled = true;
+            this.cboCategoria.Items.AddRange(new object[] {
+            "GENERADA",
+            "ANULADA"});
+            this.cboCategoria.Location = new System.Drawing.Point(316, 30);
+            this.cboCategoria.Name = "cboCategoria";
+            this.cboCategoria.Size = new System.Drawing.Size(121, 21);
+            this.cboCategoria.TabIndex = 109;
+            // 
+            // txtRequerimiento
+            // 
+            this.txtRequerimiento.Location = new System.Drawing.Point(79, 31);
+            this.txtRequerimiento.Name = "txtRequerimiento";
+            this.txtRequerimiento.Size = new System.Drawing.Size(137, 20);
+            this.txtRequerimiento.TabIndex = 138;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(15, 34);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 13);
+            this.label6.TabIndex = 137;
+            this.label6.Text = "Codigo:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(245, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 139;
+            this.label1.Text = "Categoria:";
+            // 
             // frmListadoReqCotizacion
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(594, 315);
+            this.ClientSize = new System.Drawing.Size(638, 315);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.groupBox6);
@@ -175,13 +199,15 @@
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button btnBuscar;
-        private System.Windows.Forms.TextBox txtDescripcion;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.GroupBox groupBox6;
         private System.Windows.Forms.Label lblBusqueda;
         private System.Windows.Forms.DataGridViewLinkColumn codRequerimiento;
         private System.Windows.Forms.DataGridViewTextBoxColumn desCategoria;
         private System.Windows.Forms.DataGridViewTextBoxColumn fechaRegistro;
         private System.Windows.Forms.DataGridViewTextBoxColumn observacion;
+        private System.Windows.Forms.ComboBox cboCategoria;
+        private System.Windows.Forms.TextBox txtRequerimiento;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label1;
     }
 }
