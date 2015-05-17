2b1eb168f223f1ffc0949c1caf87f4b6e6962d5e
luisfp89 <luisfp89@gmail.com>
2015-05-17 15:31:59 -0500

    Business Compra

---
+++ ProyectoETNA/ETNA.SGI.Bussiness/Compras/BRequerimientoCompra.cs
@@ -0,0 +1,33 @@
+﻿using ETNA.SGI.Data.Compras;
+using ETNA.SGI.Entity.Compras;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+namespace ETNA.SGI.Bussiness.Compras
+{
+    public class BRequerimientoCompra
+    {
+
+        private DRequerimientoCompra dRequerimientoCompra = DRequerimientoCompra.getInstance();
+
+        private static BRequerimientoCompra bRequerimientoCompra;
+
+        public static BRequerimientoCompra getInstance()
+        {
+            if (bRequerimientoCompra == null)
+            {
+                bRequerimientoCompra = new BRequerimientoCompra();
+            }
+            return bRequerimientoCompra;
+        }
+
+        public DataTable ObtenerListadoRequerimientoCompraCotizacion(ERequerimientoCompra eRequerimientoCompra)
+        {
+            return dRequerimientoCompra.DGetAllRequerimientoCompraCotizacion(eRequerimientoCompra);
+        }
+
+
+    }
+}
